1f1d0b5052b567ee83c5ae31d3ad5efd654ce4cd
Peter Polena <peter.polena@moon42.com>
2023-10-14 01:36:58 +0200

    Implement combat action function

--- src/entities/entity.ts
+++ src/entities/entity.ts
@@ -1,4 +1,5 @@
-import EntityType from '@/entities/entityType';
+import EntityType from '@/src/entities/entityType';
+import { PositionXY } from '../types/positionXY';
 
 interface Entity {
   /**
@@ -10,13 +11,33 @@ interface Entity {
    * Hit points can be either a static number or a string on which we randomly generate
    * For example, player HP: 12, monster hp: 1d8+1 (these are interchangeable)
    */
-  hitPoints: string | number;
+  hitPoints: number;
 
   /**
    * Damage is a random generated number
    * For example 1d6
    */
   damage: string;
+
+  /**
+   * Range of the character's weapon on the grid, minimum 1
+   */
+  weaponRange: number;
+
+  /**
+   * The number of actions the character can take during a turn
+   */
+  attacksLeft: number;
+
+  /**
+   * Range of the character's movement on the grid, minimum 1
+   */
+  movementRange: number;
+
+  /**
+   * The number of cells the character can move during a turn
+   */
+  movementsLeft: number;
 
   /**
    * Or AC for shorthand, determines the defense of a certain entity
@@ -29,6 +50,21 @@ interface Entity {
    * It can also be used to determine if it's a monster entity - entityType != EntityType.PLAYER
    */
   entityType: EntityType;
+
+  /**
+   * The position of the entity on the grid.
+   */
+  position: PositionXY;
+
+  /**
+   * The attack strategy of the the character
+   */
+  strategy: Strategy;
+
+  /**
+   * The last entity that attacked this entity
+   */
+  lastAttacker?: Entity;
 }
 
 export default Entity;
--- src/utils/combat.ts
+++ src/utils/combat.ts
@@ -1,9 +1,66 @@
 import Entity from '@/src/entities/entity';
 import { roll } from '@/src/utils/diceSimulator';
+import { PositionXY } from '../types/positionXY';
 
 const d20: string = '1d20';
 
-export function attack(attacker: Entity, defender: Entity) {
+export function action(attacker: Entity, defenders: Entity[]) {
+  const distanceMap = getDistanceMap(attacker, defenders);
+
+  let target: Entity;
+  switch (attacker.strategy) {
+    case Strategy.NEAREST:
+      target = getNearestTarget(distanceMap);
+      break;
+    case Strategy.LOWEST_HEALTH:
+      target = getLowestHealthTarget(defenders);
+      break;
+    default:
+      target = defenders[roll('1d' + (defenders.length - 1))];
+  }
+
+  const targetDistance = distanceMap.get(target)!;
+
+  if (attacker.attacksLeft > 0) {
+    if (attacker.weaponRange >= targetDistance) {
+      attack(attacker, target);
+      attacker.attacksLeft--;
+      if (target.hitPoints <= 0) {
+        defenders = defenders.filter((defender) => defender !== target);
+      }
+      action(attacker, defenders);
+    } else if (attacker.movementsLeft > 0) {
+      move(attacker, target);
+      attacker.movementsLeft--;
+      action(attacker, defenders);
+    }
+  }
+}
+
+function getDistanceMap(attacker: Entity, defenders: Entity[]): Map<Entity, number> {
+  let distanceMap = new Map<Entity, number>();
+  defenders.forEach((defender: Entity) => {
+    distanceMap.set(defender, Math.max(attacker.position.x - defender.position.x, attacker.position.y - defender.position.y));
+  });
+  return distanceMap;
+}
+
+function getNearestTarget(distanceMap: Map<Entity, number>): Entity {
+  let closestDefender: Entity;
+  distanceMap.forEach((distance, defender) => {
+    if (!closestDefender || (distanceMap && closestDefender && distanceMap.get(closestDefender)! > distance)) {
+      closestDefender = defender;
+    }
+  });
+  return closestDefender!;
+}
+
+function getLowestHealthTarget(defenders: Entity[]): Entity {
+  return defenders.sort((a, b) => (a.hitPoints < b.hitPoints ? -1 : a.hitPoints > b.hitPoints ? 1 : 0))[0];
+}
+
+function attack(attacker: Entity, defender: Entity) {
+  attacker.attacksLeft;
   console.log(attacker.name, 'attacks', defender.name);
 
   const attackRoll = roll(d20);
@@ -23,6 +80,49 @@ export function attack(attacker: Entity, defender: Entity) {
 
   console.log(attackRoll, 'is a hit');
   damage(attacker.damage, false, defender);
+}
+
+function move(attacker: Entity, target: Entity) {
+  const attackerPosition = attacker.position;
+  const defenderPosition = target.position;
+
+  // if target is diagonal to attacker
+  if (defenderPosition.x - attackerPosition.x === defenderPosition.y - attackerPosition.y) {
+    diagonalMovement(attackerPosition, defenderPosition);
+  } else if (defenderPosition.x - attackerPosition.x !== 0 && defenderPosition.y - attackerPosition.y === 0) {
+    xAxisMovement(attackerPosition, defenderPosition);
+  } else {
+    yAxisMovement(attackerPosition, defenderPosition);
+  }
+}
+
+function diagonalMovement(attackerPosition: PositionXY, defenderPosition: PositionXY) {
+  if (defenderPosition.x > attackerPosition.x) {
+    attackerPosition.x++;
+  } else {
+    attackerPosition.x--;
+  }
+  if (defenderPosition.y > attackerPosition.y) {
+    attackerPosition.y++;
+  } else {
+    attackerPosition.y--;
+  }
+}
+
+function xAxisMovement(attackerPosition: PositionXY, defenderPosition: PositionXY) {
+  if (defenderPosition.x > attackerPosition.x) {
+    attackerPosition.x++;
+  } else {
+    attackerPosition.x--;
+  }
+}
+
+function yAxisMovement(attackerPosition: PositionXY, defenderPosition: PositionXY) {
+  if (defenderPosition.y > attackerPosition.y) {
+    attackerPosition.y++;
+  } else {
+    attackerPosition.y--;
+  }
 }
 
 // If we want to abide by the official 3.5 rules
