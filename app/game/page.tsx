bf70338bd033fbcac5112e51b66ba2343865edac
Istvan Erdos <istvan.erdos@moon42.com>
2023-10-13 18:55:08 +0200

    Place characters on grid

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,15 +1,35 @@
+"use client"
+
 import React from "react";
 import Battlefield from "../components/Battlefield";
 import EntityStatsHud from "../components/EntityStatsHud";
 import GameLog from "../components/GameLog";
 import { GlobalContextProvider } from "../Context/gameState";
+import Player from "@/entities/player";
+import Goblin from "@/entities/goblin";
 
 function Game() {
+  const players = [
+    new Player('Józsi'),
+    new Player('Béla'),
+    new Player('Norbi'),
+    new Player('Szidi'),
+    new Player('Dani'),
+  ]
+
+  const goblins = [
+    new Goblin(),
+    new Goblin(),
+    new Goblin(),
+    new Goblin(),
+    new Goblin(),
+  ]
+
   return (
     <div className="h-screen flex">
       <GlobalContextProvider>
         <EntityStatsHud />
-        <Battlefield />
+        <Battlefield players={players} enemies={goblins} />
         <GameLog />
       </GlobalContextProvider>
     </div>
