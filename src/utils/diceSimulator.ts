35414c53e828f484614a135d36f1e6200081ef06
Peter Polena <peter.polena@moon42.com>
2023-10-13 14:23:14 +0200

    refactor

--- src/utils/diceSimulator.ts
+++ src/utils/diceSimulator.ts
@@ -1,17 +1,16 @@
 export function roll(diceRollExpression: string): number {
-  const numberOfRolls = parseInt(diceRollExpression.toLowerCase().split('d')[0]);
+  const expression = diceRollExpression.toLowerCase();
   const plus = diceRollExpression.indexOf('+') > -1 ? true : false;
   const minus = diceRollExpression.indexOf('-') > -1 ? true : false;
+  const splitOnD = expression.split('d');
+  const numberOfRolls = parseInt(splitOnD[0]);
   const regex = new RegExp('[+-]');
-  const numberOfDiceSides =
-    plus || minus
-      ? parseInt(diceRollExpression.toLowerCase().split('d')[1].split(regex)[0])
-      : parseInt(diceRollExpression.toLowerCase().split('d')[1]);
-  const extra = plus || minus ? parseInt(diceRollExpression.toLowerCase().split('d')[1].split(regex)[1]) : 0;
+  const splitOnPlusMinus = splitOnD[1].split(regex);
+  const numberOfDiceSides = plus || minus ? parseInt(splitOnPlusMinus[0]) : parseInt(splitOnD[1]);
+  const extra = plus || minus ? parseInt(splitOnPlusMinus[1]) : 0;
   let result = 0;
   for (let i = 0; i < numberOfRolls; i++) {
     const rollResult = Math.floor(Math.random() * numberOfDiceSides + 1);
-
     result += rollResult;
   }
   if (plus) {
